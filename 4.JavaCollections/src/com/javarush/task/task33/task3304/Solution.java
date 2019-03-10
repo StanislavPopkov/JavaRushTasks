105974bbca5acf050ff12e5c4762e3184a594712
StanislavPopkov <st.popkov@gmail.com>
2019-03-10 18:53:37 +0300

    JSON Disable Annotations

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task33.task3304;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+
+/* 
+Конвертация из одного класса в другой используя JSON
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(byteArrayOutputStream, one);
+
+        System.out.println(byteArrayOutputStream.toString());
+        return  objectMapper.readValue(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), resultClassObject);
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))
+    public static class First {
+        public int i;
+        public String name;
+
+        public First() {
+
+        }
+    }
+
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=Second.class, name="second"))
+    public static class Second {
+        public int i;
+        public String name;
+
+        public Second() {
+
+        }
+    }
+}
